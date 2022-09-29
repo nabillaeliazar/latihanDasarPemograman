d713421ea2542c049d89c29121c2ff428b9b4e54
nabillaeliazar <114629746+nabillaeliazar@users.noreply.github.com>
2022-09-28 21:36:28 -0700

    Add files via upload

---
+++ aplikasibiodata.py
@@ -0,0 +1,19 @@
+# aplikasi biodata create by nabilla eliazar
+
+# masukan nama
+nama = input("masukan nama anda")
+
+# print\ output nama
+print("nama anda adalah: ", nama)
+
+# masukan kelas
+kelas = input("masukan kelas anda")
+
+# print \ output kelas
+print("kelas:", kelas)
+
+# masukan prodi
+prodi = input("masukan prodi")
+
+# print \ output prodi
+print("program studi: ", prodi)
